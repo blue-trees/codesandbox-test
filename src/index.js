00ce9b199c4872f8a9f5207dd48dc373d2560cd7
blue-trees <51107771+blue-trees@users.noreply.github.com>
2021-06-01 15:18:31 +0900

    JS学習

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const name = "きよ";
+const age = 29;
+
+const message1 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message1);
+
+/**
+ * アロー関数
+ */
+
+const func2 = (str) => {
+  return str;
+};
+
+console.log(func2("func2です"));
